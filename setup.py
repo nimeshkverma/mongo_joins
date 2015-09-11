fcd6571fecd5561db00c5c5fe878ec67a2df9808
Utsav Tiwary <utsav.tiwary2011@gmail.com>
2015-09-12 02:39:23 +0530

    Minor naming changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='mongojoin',
       version='1.0',
       description='Python library for performing joins on MongoDB collections',
-      author='Nimesh K. (supported ably by DrCricket)',
+      author='Nimesh Kiran (supported ably by DrCricket)',
       author_email='nimesh.aug11@gmail.com',
       url='https://github.com/nimeshkverma/mongo_joins',
       py_modules=['mongojoin'],
